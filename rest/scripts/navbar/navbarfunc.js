89696bf2ae3a16f97aa8f049b041f1d03c76fde0
Gaurav Achhra <achhragaurav@gmail.com>
2021-05-31 15:26:43 +0530

    deploy

--- rest/scripts/navbar/navbarfunc.js
+++
@@ -1,9 +0,0 @@
-export const navBarFunc = () => {
-  const navBarButton = document.getElementById("menuBtn");
-  const menu = document.querySelector(".actual-menu");
-  navBarButton.addEventListener("click", () => {
-    menu.classList.toggle("menu-new");
-    document.body.classList.toggle("new-body-navbar");
-    document.querySelector("html").classList.toggle("new-body-navbar");
-  });
-};
